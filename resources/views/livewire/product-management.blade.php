590f2f04139581643684ecf7eccf0630f52b402a
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-01-22 17:29:04 +0530

    app

--- resources/views/livewire/product-management.blade.php
+++ resources/views/livewire/product-management.blade.php
@@ -75,7 +75,7 @@
                         </div>
                     @endif
 
-                    <table class="table table-responsive table-bordered">
+                    <table class="table table-responsive">
                         <thead>
                             <tr>
                                 <th>No.</th>
