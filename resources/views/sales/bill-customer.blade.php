da452916f318cbe2b53349191cdd0d3232c0b65e
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-01-26 04:24:44 +0530

    app

--- resources/views/sales/bill-customer.blade.php
+++ resources/views/sales/bill-customer.blade.php
@@ -20,7 +20,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-2 col-sm-6 col-12">
+            <div class="col-lg-3 col-sm-6 col-12">
                 <div class="form-group">
                     <label>Invoice Date </label>
                     <div class="input-groupicon">
