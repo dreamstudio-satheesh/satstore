a8cc6e433276b3e98742924c58dec048759df514
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-01-28 10:22:19 +0530

    app

--- resources/views/bills/print.blade.php
+++ resources/views/bills/print.blade.php
@@ -1,206 +1,146 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <title>POS Receipt</title>
+    <style>
+        @media print {
+            html, body {
+                width: 80mm;
+                height: 100%;
+                position: absolute;
+            }
+            .page-break {
+                display: block;
+                page-break-before: always;
+            }
+        }
 
-  <meta charset="UTF-8">
-  <title> POS Receipt</title>
+        #invoice-POS {
+            box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
+            padding: 2mm;
+            margin: 0 auto;
+            width: 80mm;
+            background: #FFF;
+        }
 
-  <style>
+        #invoice-POS h1 {
+            font-size: 1.5em;
+            color: #222;
+        }
 
-@media print {
-   html, body {
-    width: 80mm;
-    height:100%;
-    position:absolute;
-   }
+        #invoice-POS h2 {
+            font-size: .9em;
+        }
 
-   .page-break { display: block; page-break-before: always; }
-}
+        #invoice-POS h3 {
+            font-size: 1.2em;
+            font-weight: 300;
+            line-height: 2em;
+        }
 
-  #invoice-POS {
-  box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
-  padding: 2mm;
-  margin: 0 auto;
-  width: 80mm;
-  background: #FFF;
-}
-#invoice-POS ::selection {
-  background: #f31544;
-  color: #FFF;
-}
-#invoice-POS ::moz-selection {
-  background: #f31544;
-  color: #FFF;
-}
-#invoice-POS h1 {
-  font-size: 1.5em;
-  color: #222;
-}
-#invoice-POS h2 {
-  font-size: .9em;
-}
-#invoice-POS h3 {
-  font-size: 1.2em;
-  font-weight: 300;
-  line-height: 2em;
-}
-#invoice-POS p {
-  font-size: .7em;
-  color: #666;
-  line-height: 1.2em;
-}
-#invoice-POS #top, #invoice-POS #mid, #invoice-POS #bot {
-  /* Targets all id with 'col-' */
-  border-bottom: 1px solid #EEE;
-}
-#invoice-POS #top {
-  min-height: 100px;
-}
-#invoice-POS #mid {
-  min-height: 80px;
-}
-#invoice-POS #bot {
-  min-height: 50px;
-}
-#invoice-POS #top .logo {
-  height: 60px;
-  width: 160px;
-  background: url(assets/img/logo.png) no-repeat;
-  background-size: 160px 60px;
-}
+        #invoice-POS p {
+            font-size: .7em;
+            color: #666;
+            line-height: 1.2em;
+        }
 
-#invoice-POS .info {
-  display: block;
-  margin-left: 0;
-}
-#invoice-POS .title {
-  float: right;
-}
-#invoice-POS .title p {
-  text-align: right;
-}
-#invoice-POS table {
-  width: 100%;
-  border-collapse: collapse;
-}
-#invoice-POS .tabletitle {
-  font-size: .5em;
-  background: #EEE;
-}
-#invoice-POS .service {
-  font-size: 1.2em;
-  border-bottom: 1px solid #EEE;
-}
-#invoice-POS .item {
-  width: 42mm;
-}
-#invoice-POS .itemtext {
-  font-size: .5em;
-}
-#invoice-POS #legalcopy {
-  margin-top: 5mm;
-}
+        #invoice-POS .info {
+            display: block;
+            margin-left: 0;
+        }
 
+        #invoice-POS table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        #invoice-POS .tabletitle {
+            font-size: .5em;
+            background: #EEE;
+        }
+
+        #invoice-POS .service {
+            font-size: 1.2em;
+            border-bottom: 1px solid #EEE;
+        }
+
+        #invoice-POS .item {
+            width: 42mm;
+        }
+
+        #invoice-POS .itemtext {
+            font-size: .5em;
+        }
+
+        #invoice-POS #legalcopy {
+            margin-top: 5mm;
+        }
     </style>
-
-  <script>
-  window.console = window.console || function(t) {};
-</script>
-
-
-
-  <script>
-  if (document.location.search.match(/type=embed/gi)) {
-    window.parent.postMessage("resize", "*");
-  }
-</script>
-
-
 </head>
 
-<body translate="no" >
+<body>
+    <div id="invoice-POS">
+        <center id="top">
+            <div class="logo"></div>
+            <div class="info">
+                <h2>SAT Sweets</h2>
+                <p>
+                    3/147 Karunaipalayam Pirivu,<br>
+                    Covai-Tiruchy Main Road,<br>
+                    Kangeyam -638701
+                </p>
+            </div>
+        </center>
 
+        <div id="mid">
+            <div class="info">
+                <p>Bill Number: {{ sprintf('%04d', $bill->id) }}</p>
+                <p>Bill Date: {{ $bill->created_at->format('d-m-Y') }}</p>
+                <h2>Billing Address:</h2>
+                <p>{{ $bill->customer->name ?? 'Walk-In Customer' }}</p>
+                <p>{{ $bill->customer->address ?? '-' }}</p>
+            </div>
+        </div>
 
-  <div id="invoice-POS">
+        <div id="bot">
+            <div id="table">
+                <table>
+                    <tr class="tabletitle">
+                        <td><h2>Item</h2></td>
+                        <td><h2>Price</h2></td>
+                        <td><h2>Qty</h2></td>
+                        <td><h2>Sub Total</h2></td>
+                    </tr>
+                    @foreach ($bill->items as $item)
+                        <tr class="service">
+                            <td><p class="itemtext">{{ $item->product->name_tamil }}</p></td>
+                            <td><p class="itemtext">{{ number_format($item->price, 2) }}</p></td>
+                            <td><p class="itemtext">{{ $item->quantity }}</p></td>
+                            <td><p class="itemtext">{{ number_format($item->price * $item->quantity, 2) }}</p></td>
+                        </tr>
+                    @endforeach
+                    <tr class="tabletitle">
+                        <td></td>
+                        <td></td>
+                        <td class="Rate"><h2>Tax</h2></td>
+                        <td class="payment"><h2>{{ number_format($bill->items->sum('cgst') + $bill->items->sum('sgst'), 2) }}</h2></td>
+                    </tr>
+                    <tr class="tabletitle">
+                        <td></td>
+                        <td></td>
+                        <td class="Rate"><h2>Total</h2></td>
+                        <td class="payment"><h2>{{ number_format($bill->final_amount, 2) }}</h2></td>
+                    </tr>
+                </table>
+            </div>
 
-    <center id="top">
-      <div class="logo"></div>
-      <div class="info"> 
-        <h2>SAT Sweets</h2>
-        <p> 
-          3/147 Karunaipalayam Pirivu,</br>
-          Covai-Tiruchy Main Road, </br>
-          Kangeyam -638701</br>
-      </p>
-      </div><!--End Info-->
-    </center><!--End InvoiceTop-->
-
-    <div id="mid">
-      <div class="info">
-        <p> Bill Number : {{ sprintf('%04d',$invoice->invoice_number) }}</p>
-
-        <p> Bill Date : {{  \Carbon\Carbon::parse($invoice->date)->format('d-m-Y') }}</p>
-
-        <h2>Billing Address :</h2>
-        <p> {{ $invoice->customer->name }} </p>
-        <p> 
-            {{ $invoice->customer->address }}</br>
-        </p>
-      </div>
-    </div><!--End Invoice Mid-->
-
-    <div id="bot">
-
-                    <div id="table">
-                        <table>
-                            <tr class="tabletitle">
-                                <td class=""><h2>Item</h2></td>
-                                <td class=""><h2>Price</h2></td>
-                                <td class=""><h2>Qty</h2></td>
-                                <td class=""><h2>Sub Total</h2></td>
-                            </tr>
-
-                            @foreach ($invoice->invoice_items as $item)
-                              <tr class="service">
-                                <td class=""><p class="itemtext">{{ $item->name }}</p></td>                                
-                                <td class=""><p class="itemtext">{{ ($item->price - $item->gstamount) }}</p></td>
-                                <td class=""><p class="itemtext">{{ $item->quantity }}</p></td>
-                                <td class=""><p class="itemtext">{{ ($item->price - $item->gstamount)* $item->quantity }}</p></td>
-                            </tr>                                
-                            @endforeach                          
-
-
-                            <tr class="tabletitle">
-                                <td></td>
-                                <td></td>
-                                <td class="Rate"><h2>tax</h2></td>
-                                <td class="payment"><h2>{{ $invoice->taxamount }}</h2></td>
-                            </tr>
-
-                            <tr class="tabletitle">
-                                <td></td>
-                                <td></td>
-                                <td class="Rate"><h2>Total</h2></td>
-                                <td class="payment"><h2>{{ $invoice->total }}</h2></td>
-                            </tr>
-
-                        </table>
-                    </div><!--End Table-->
-
-                    <div id="legalcopy">
-                        <p class="legal"><strong>Thank you for your business!</strong> satsweets. 
-                        </p>
-                    </div>
-
-                </div><!--End InvoiceBot-->
-  </div><!--End Invoice-->
-
-
-
-
-
-
+            <div id="legalcopy">
+                <p class="legal"><strong>Thank you for your business!</strong> SAT Sweets.</p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
